6abf7cf774f2554d5b1c79aa93968dd0ae1becfe
wojtan222 <wojciechxcybulski@gmail.com>
2023-10-05 15:46:38 +0200

    Initial commit

--- src/test/java/pl/coderslab/shop/ShopTest.java
+++ src/test/java/pl/coderslab/shop/ShopTest.java
@@ -1,2 +1,11 @@
-package pl.coderslab.shop;public class ShopTest {
+package pl.coderslab.shop;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "C:\\CodersLab\\EXAM-WORK2\\src\\Cucumber\\Features", plugin = {"pretty", "html:out.html"})
+
+public class ShopTest {
 }
